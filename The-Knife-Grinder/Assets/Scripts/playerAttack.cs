4c18bb7507e92d56e2aea9e61c1b78a20de26f97
Amehi <71913275+Amehi@users.noreply.github.com>
2021-11-27 21:19:07 +0800

    update attack

--- The-Knife-Grinder/Assets/Scripts/playerAttack.cs
+++ The-Knife-Grinder/Assets/Scripts/playerAttack.cs
@@ -8,6 +8,9 @@ public class playerAttack : MonoBehaviour
 
     private Animator animator;
     private Rigidbody rb;
+
+    private float coolingTimer = 0.6f;
+    private float currentTime = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,13 +23,14 @@ public class playerAttack : MonoBehaviour
     void Update()
     {
         attack();
-
+        Delay();
     }
-    void Delay(int t)
+    void Delay()
     {
-        for(int i = t; i>=0; i--)
+        if (currentTime < coolingTimer)
         {
-            ;
+            Debug.Log(currentTime);
+            currentTime += Time.deltaTime;
         }
     }
     private void attack()
@@ -34,22 +38,23 @@ public class playerAttack : MonoBehaviour
 
         if (transform.position.y >= -0.596f)
             return;
-
+        if (currentTime < coolingTimer)
+            return;
         if (Input.GetMouseButtonDown(1))
         {
             if (rb.velocity.magnitude > 4 && !animator.GetCurrentAnimatorStateInfo(0).IsName("flying_kick") && !animator.GetCurrentAnimatorStateInfo(0).IsName("punch"))
             {
                 animator.Play("flying_kick");
+                currentTime = -1.1f;
             }
 
             else if (!animator.GetCurrentAnimatorStateInfo(0).IsName("punch") && !animator.GetCurrentAnimatorStateInfo(0).IsName("flying_kick"))
             { 
                 animator.Play("punch");
+                currentTime = 0.0f;
             }
 
         }
-
-
     }
 
 
