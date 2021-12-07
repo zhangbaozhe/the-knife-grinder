8fb8132911f9eadd9c8a746cfe5a069bf7c1949b
Aaron <119010382@link.cuhk.edu.cn>
2021-12-08 06:06:11 +0800

    add_map

---
+++ VolumetricLighting/Assets/Map/Script/CameraMove.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public Transform playerCam;
+    public static CameraMove Instance = null;
+
+    private Vector3 dirVector3;
+    private Vector3 rotaVector3;
+    private float paramater = 0.1f;
+
+
+    //Rotation and look
+    private float xRotation;
+    private float sensitivity = 50f;
+    private float sensMultiplier = 1f;
+    void Awake()
+{
+    Instance = this;
+}
+
+    private void Start()
+    {
+        rotaVector3 = transform.localEulerAngles;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+            Look();
+
+        //移动
+        dirVector3 = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.z = 3;
+            else dirVector3.z = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.z = -3;
+            else dirVector3.z = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.x = -3;
+            else dirVector3.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.x = 3;
+            else dirVector3.x = 1;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.y = -3;
+            else dirVector3.y = -1;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            if (Input.GetKey(KeyCode.LeftShift)) dirVector3.y = 3;
+            else dirVector3.y = 1;
+        }
+        transform.Translate(dirVector3 * paramater, Space.Self);
+        //限制摄像机范围
+    }
+
+    private float desiredX;
+    private void Look()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.fixedDeltaTime * sensMultiplier;
+
+        //Find current look rotation
+        Vector3 rot = playerCam.transform.localRotation.eulerAngles;
+        desiredX = rot.y + mouseX;
+
+        //Rotate, and also make sure we dont over- or under-rotate.
+        xRotation -= mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //Perform the rotations
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, desiredX, 0);
+        //playerCam.transform.localRotation = Quaternion.Euler(0, desiredX, 0);
+    }
+}
+
