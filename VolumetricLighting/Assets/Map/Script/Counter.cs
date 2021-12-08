0e071f9ace9bcb6233f08063e5453aed080bf7fb
Aaron <119010382@link.cuhk.edu.cn>
2021-12-08 21:22:20 +0800

    improve_map

---
+++ VolumetricLighting/Assets/Map/Script/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+	private Text TimeText;
+	public float times = 225;
+	private int s;//定义一个秒
+
+	void Start()
+	{
+		TimeText = GameObject.Find("TimeText").GetComponent<Text>();
+	}
+
+	void Update()
+	{
+		//计时器完成倒计时的功能
+		times -= Time.deltaTime;
+		s = (int)times % 225; //小数转整数 
+		TimeText.text = s.ToString();
+	}
+}
---
+++ VolumetricLighting/Assets/Map/Script/Flash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash : MonoBehaviour
+{
+    public GameObject Light;
+    public GameObject timeManager;
+    public float interval = 1f;
+    public float startTime = 225f;
+    public float endTime = 195f;
+
+    private bool isFlash = false;
+    private float _time; 
+    float nextFlashTime = 225;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time = timeManager.GetComponent<Counter>().times;
+        if (_time >= endTime && _time <= startTime)
+        {
+            if (isFlash && _time < nextFlashTime)
+            {
+                Light.GetComponent<TubeLight>().m_Color = Color.red;
+                isFlash = false;
+                nextFlashTime = _time - interval;
+            }
+            else if (_time < nextFlashTime)
+            {
+                Light.GetComponent<TubeLight>().m_Color = Color.white;
+                isFlash = true;
+                nextFlashTime = _time - interval;
+            }
+        }
+        if(_time < endTime)
+        {
+            Light.GetComponent<TubeLight>().m_Color = Color.red;
+        }
+
+    }
+}
