56b3287aa411dfff896884ded7e43c5bf511fe33
Aaron <119010382@link.cuhk.edu.cn>
2021-12-09 02:53:04 +0800

    add floor dropping

--- VolumetricLighting/Assets/Map/Script/Counter.cs
+++ VolumetricLighting/Assets/Map/Script/Counter.cs
@@ -2,23 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
-public class Counter : MonoBehaviour
+public class Counter : NetworkBehaviour
 {
 	private Text TimeText;
 	public float times = 225;
+	[SyncVar]
 	private int s;//定义一个秒
+
+	public GameObject[] firstLevel = new GameObject[5];
+	public GameObject[] secondLevel = new GameObject[16];
 
 	void Start()
 	{
 		TimeText = GameObject.Find("TimeText").GetComponent<Text>();
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
 		//计时器完成倒计时的功能
 		times -= Time.deltaTime;
 		s = (int)times % 225; //小数转整数 
 		TimeText.text = s.ToString();
+
+		// Baozhe: first level shaking and falling
+		if (s >= 155 && s < 165)
+        {
+			if (s == 155)
+            {
+				for (int i = 0; i < 5; i++)
+                {
+					firstLevel[i].GetComponent<fallingApart>().isFalling = true;
+                }
+			}
+			else
+            {
+				for (int i = 0; i < 5; i++)
+                {
+					firstLevel[i].GetComponent<fallingApart>().isShaking = true;
+                }
+            }
+
+        }
+
+		// Baozhe: second level shaking and falling
+		if (s >= 95 && s < 105)
+        {
+			if (s == 95)
+            {
+				for (int i = 0; i < 16; i++)
+                {
+					secondLevel[i].GetComponent<fallingApart>().isFalling = true;
+                }
+            }
+			else
+            {
+				for (int i = 0; i < 16; i++)
+                {
+					secondLevel[i].GetComponent<fallingApart>().isShaking = true;
+                }
+            }
+        }
 	}
 }
---
+++ VolumetricLighting/Assets/Scripts/fallingApart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallingApart : MonoBehaviour
+{
+    public bool isFalling = false;
+    public bool isShaking = false;
+    public Transform tr;
+
+    private int i = -1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isFalling)
+        {
+            tr.gameObject.SetActive(false); ;
+        }
+
+        if (isShaking)
+        {
+            tr.position = Vector3.Lerp(tr.position, tr.position + new Vector3(0, i * 3), Time.deltaTime * 5);
+            i = -i;
+        }
+    }
+}
