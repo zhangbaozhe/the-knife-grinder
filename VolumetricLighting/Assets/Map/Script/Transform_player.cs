9fa83dd076b5001190b41fe992ed24885fb15a67
Amehi <71913275+Amehi@users.noreply.github.com>
2021-12-12 02:48:00 +0800

    fix shader

--- VolumetricLighting/Assets/Map/Script/Transform_player.cs
+++ VolumetricLighting/Assets/Map/Script/Transform_player.cs
@@ -11,6 +11,7 @@ public class Transform_player : MonoBehaviour
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
---
+++ VolumetricLighting/Assets/Scripts/PlayerRim.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRim : MonoBehaviour
+{
+
+    public Material source;
+    private Material myMat;
+    
+    void Start()
+    {
+        
+        //smr = GetComponent<SkinnedMeshRenderer>();
+        myMat = new Material(source);
+        Debug.Log(myMat.shader.name);
+        myMat.SetColor("_AtmoColor", Color.blue);
+        Debug.Log(myMat.shader.FindPropertyIndex("_AtmoColor"));
+            
+        Material[] Mat = new Material[2] { myMat, myMat };
+        transform.GetComponent<Renderer>().materials = Mat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
