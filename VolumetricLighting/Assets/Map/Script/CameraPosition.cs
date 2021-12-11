12bfe38328cf5e08b59130bb7f275b9fe45165f4
Aaron <119010382@link.cuhk.edu.cn>
2021-12-12 00:08:52 +0800

    time

---
+++ VolumetricLighting/Assets/Map/Script/CameraPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public Transform camera_position;
+    public bool isEnding = true;
+    private bool hasTransformed = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isEnding && !hasTransformed)
+        {
+            camera_position.position = new Vector3(0f, 129f, 303f);
+            hasTransformed = true;
+            camera_position.Rotate(Vector3.left * 11) ;
+
+        }
+    }
+}
